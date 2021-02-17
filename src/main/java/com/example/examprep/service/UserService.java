2333051161c43ecfbb6196a663ad1dd98c7aa9e9
Denis <d_lozanov@abv.bg>
2021-02-17 20:12:34 +0200

    Starting

---
+++ src/main/java/com/example/examprep/service/UserService.java
@@ -0,0 +1,7 @@
+package com.example.examprep.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface UserService {
+}
---
+++ src/main/java/com/example/examprep/service/impl/UserServiceImpl.java
@@ -0,0 +1,6 @@
+package com.example.examprep.service.impl;
+
+import com.example.examprep.service.UserService;
+
+public class UserServiceImpl implements UserService {
+}
